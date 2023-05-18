97fa87d522b0662dc5a7242260007cb681118f79
GitUser034 <133963938+GitUser034@users.noreply.github.com>
2023-05-18 15:36:50 +0530

    Update DevClass.java
    Update possible so done

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -3,5 +3,6 @@ public class DevClass {
       public static void main(String[] args) {
             System.out.println("Inside Dev Class");
             //Neil changed this
+            //this is the update
       }
 }
